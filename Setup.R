fa322a218fd24cf64a82aaa5b42d2417ff0425fc
gracegk <grgkim@bu.edu>
2019-11-11 15:58:11 -0500

    constructing dataset. for USA 2017 jan for now

--- Setup.R
+++ Setup.R
@@ -6,7 +6,7 @@ library(lubridate)
 library(dplyr)
 library(data.table)
 
-# my personal user id: 122043448
+# my personal Spotify user id: 122043448
 # id/secret: for this project
 id <- 'ee9f6edc68c145e582d736fa7ddf0b6c'
 secret <- 'a0b0680390e14015862964b209e798cc'
@@ -15,19 +15,21 @@ Sys.setenv(SPOTIFY_CLIENT_SECRET = secret)
 access_token <- get_spotify_access_token()
 
 
-my_playlists <- get_user_playlists("122043448")
-my_playlists2 <- my_playlists %>%
-  filter(name %in% c('United States Top 50'))
-tracks <- get_playlist_tracks(my_playlists2$id)
-features <- get_track_audio_features(tracks)
-get_track_audio_features()
-
-
+## Building the Data Set
+# Read in csv file and clean it up
 USA_2017_jan <- read.csv("~/Documents/MSSP Fall 2019/MA678/Bops_by_Country/Raw_Data/regional-us-weekly-2016-12-30--2017-01-06.csv")
 names(USA_2017_jan) <- lapply(USA_2017_jan[1, ], as.character)
 USA_2017_jan <- USA_2017_jan[-1,] 
-USA_2017_jan$trackID <- substring(USA_2017_jan$URL, 32)
+USA_2017_jan$id <- substring(USA_2017_jan$URL, 32)
 
-USA2017jan_1 <- get_track_audio_features(ids = USA_2017_jan$trackID[1:100])
+# Add track feature data
+# get_track_audio_features() limits the number of ids you can enter to 100
+USA2017jan_1 <- get_track_audio_features(ids = USA_2017_jan$id[1:100])
+USA2017jan_2 <- get_track_audio_features(ids = USA_2017_jan$id[101:200])
+USA_2017_jan_features <- rbind(USA2017jan_1, USA2017jan_2)
+USA_2017_jan <- full_join(USA_2017_jan, USA_2017_jan_features, by = 'id')
+
+
+## Make function based on process above, applicable for every csv file 
 
 
